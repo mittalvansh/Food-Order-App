709ce512f8aaa6a6c1782ec959d6f2cdfa28fee3
Vansh Mittal <mittalvansh69@gmail.com>
2023-01-31 00:26:34 +0530

    feat: Fetched Data from firestore!

--- src/components/NavBar/Header.jsx
+++ src/components/NavBar/Header.jsx
@@ -1,11 +1,57 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
+import classes from './Header.module.css';
 import mealsImage from '../../assets/Meals.jpg';
 import Cart from "../Cart/Cart";
-import classes from './Header.module.css';
 import CartContext from "../Hooks/cart-context";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faBars,
+} from "@fortawesome/free-solid-svg-icons";
+import app from "../../firebaseConfig";
+import { db } from "../../firebaseConfig";
+import { Drawer, Box, Typography, Button } from "@mui/material";
+import { collection, getDocs } from "firebase/firestore";
 
 function Header() {
     const [view, setView] = useState(false);
+    const [isopen, setIsOpen] = useState(false);
+    const [items, setItems] = useState([]);
+
+    const id = window.localStorage.getItem("uid");
+    const dbInstance = collection(db, "User" , id , "orderData");
+
+    const getData = async () => {
+        try{
+            const data = await getDocs(dbInstance);
+            const newData = data.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data(),
+            }));
+            newData.map((item) => {
+                let name = item.foodData.map((e) => {
+                    return e.name;
+                })
+                let amount = item.foodData.map((e) => {
+                    return e.amount;
+                })
+                let totalAmount = item.totalAmount;
+                let time = item.time;
+                setItems(prev => ([...prev, {
+                    name: name,
+                    amount: amount,
+                    totalAmount: totalAmount,
+                    time: time,
+                    id: item.id
+                }]));
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    useEffect(() => {
+        getData();
+    }, []);
 
     const viewHandler = () => {
         setView(true);
@@ -26,11 +72,98 @@ function Header() {
             {view && <Cart onClose={closeHandler} />}
             <header className={classes.header}>
                 <h1>Shiv Shakti Foods</h1>
-                <button className={classes.button} onClick={viewHandler}>
-                    <span>Your Cart</span>
-                    <span className={classes.badge}>{numberOfCartItems}</span>
-                </button>
+                <div className={classes.rightSide}>
+                    <button className={classes.button} onClick={viewHandler}>
+                        <span>Your Cart</span>
+                        <span className={classes.badge}>{numberOfCartItems}</span>
+                    </button>
+                    <p onClick={() => {
+                        setIsOpen(true);
+                    }}>
+                        <FontAwesomeIcon icon={faBars} />
+                    </p>
+                </div>
             </header>
+            <Drawer anchor="right" open={isopen}
+                onClose={() => {
+                    setIsOpen(false);
+                }}
+            >
+                <Box width='300px' textAlign='center'>
+                    <div style={{
+                        height: '5rem',
+                        backgroundColor: '#651212',
+                    }}></div>
+                    <Typography variant='h5' style={{
+                        padding: '1rem',
+                    }}>
+                        Shiv Shakti Foods
+                    </Typography>
+                    <div style={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                    }}>
+                        <Button 
+                            variant='contained' 
+                            style={{
+                                marginBottom: '1rem',
+                                backgroundColor: '#9f1717',
+                                color: '#fff',
+                                border: '3px solid #fff',
+                                borderRadius: '8px',
+                                height: '60px',
+                                padding: '0 4rem',
+                                fontWeight: 'bold',
+                                fontSize: '1rem',
+                                transition: 'all 0.3s ease',
+                            }}
+                        >
+                            My Profile
+                        </Button>
+                        <Button 
+                            variant='contained' 
+                            style={{
+                                marginBottom: '1rem',
+                                backgroundColor: '#9f1717',
+                                color: '#fff',
+                                border: '3px solid #fff',
+                                borderRadius: '8px',
+                                height: '60px',
+                                padding: '0 4rem',
+                                fontWeight: 'bold',
+                                fontSize: '1rem',
+                                transition: 'all 0.3s ease',
+                            }}
+                        >
+                            My Orders
+                        </Button>
+                        <Button
+                            variant='contained'
+                            style={{
+                                marginTop: '1rem',
+                                border: '2px solid #fff',
+                                backgroundColor: '#9f1717',
+                                color: '#fff',
+                                borderRadius: '4px',
+                                height: '40px',
+                                padding: '0 1rem',
+                                fontWeight: 'bold',
+                                fontSize: '0.8rem',
+                                transition: 'all 0.3s ease',
+                            }}
+                            onClick={() => {
+                                window.localStorage.removeItem('loggedIn');
+                                window.localStorage.removeItem('uid');
+                                window.location.reload();
+                            }}
+                        >
+                            Logout
+                        </Button>
+                    </div>
+                </Box>
+            </Drawer>
             <div className={classes['main-image']}>
                 <img src={mealsImage} alt="A Table full of delicious Food!" />
             </div>
