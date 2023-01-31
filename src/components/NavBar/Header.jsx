4df401a22ed91cd9b9de0cdf66e9dfe1baa7b793
Vansh Mittal <mittalvansh69@gmail.com>
2023-01-31 14:53:26 +0530

    feat: Added Order Section!

--- src/components/NavBar/Header.jsx
+++ src/components/NavBar/Header.jsx
@@ -9,13 +9,16 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import app from "../../firebaseConfig";
 import { db } from "../../firebaseConfig";
-import { Drawer, Box, Typography, Button } from "@mui/material";
+import { Drawer, Box, Typography, Button, 
+    Dialog, DialogActions, DialogContent, DialogTitle } from "@mui/material";
 import { collection, getDocs } from "firebase/firestore";
 
 function Header() {
     const [view, setView] = useState(false);
     const [isopen, setIsOpen] = useState(false);
+    const [open, setOpen] = useState(false);
     const [items, setItems] = useState([]);
+    console.log(items);
 
     const id = window.localStorage.getItem("uid");
     const dbInstance = collection(db, "User" , id , "orderData");
@@ -104,6 +107,7 @@ function Header() {
                         flexDirection: 'column',
                         alignItems: 'center',
                         justifyContent: 'center',
+                        padding: '1rem',
                     }}>
                         <Button 
                             variant='contained' 
@@ -136,6 +140,9 @@ function Header() {
                                 fontSize: '1rem',
                                 transition: 'all 0.3s ease',
                             }}
+                            onClick={() => {
+                                setOpen(true);
+                            }}
                         >
                             My Orders
                         </Button>
@@ -162,6 +169,90 @@ function Header() {
                             Logout
                         </Button>
                     </div>
+                    <Dialog
+                        open={open}
+                        onClose={() => {
+                            setOpen(false);
+                        }}
+                        aria-labelledby="alert-dialog-title"
+                        aria-describedby="alert-dialog-description"
+                    >
+                        <DialogTitle id="alert-dialog-title">{"Your Orders"}</DialogTitle>
+                        <DialogContent>
+                            {items.map((item) => {
+                                return (
+                                    <Box style={{
+                                        display: 'flex',
+                                        justifyContent: 'space-between',
+                                        alignItems: 'center',
+                                        gap: '1rem',
+                                        padding: '1rem',
+                                        border: '1px solid #9f1717',
+                                        borderRadius: '8px',
+                                        margin: '1rem',
+                                    }}
+                                    flexDirection={{ xs: 'column', sm: 'row' }}
+                                    justifyContent={{ xs: 'center', sm: 'space-between' }}
+                                    >
+                                        <Box 
+                                            style={{
+                                                display: 'flex',
+                                                flexDirection: 'column',
+                                                justifyContent: 'center',
+                                                alignItems: 'flex-start',
+                                            }}
+                                            alignItems={{ xs: 'center', sm: 'flex-start' }}
+                                        >
+                                            {item.name.map((e, i) => {
+                                                return (
+                                                    <Typography variant='h6'
+                                                        fontSize={{ xs: '0.8rem', sm: '1.2rem'}}
+                                                    >
+                                                        {e} x {item.amount[i]}
+                                                    </Typography>
+                                                )
+                                            })}
+                                        </Box>
+                                        <Box 
+                                            style={{
+                                                display: 'flex',
+                                                flexDirection: 'column',
+                                                justifyContent: 'center',
+                                                alignItems: 'flex-end',
+                                            }}
+                                        >
+                                            <Typography 
+                                                variant='h6'
+                                                fontSize={{ xs: '0.8rem', sm: '1.2rem'}}
+                                            >
+                                                {item.time.slice(0,16)}
+                                            </Typography>
+                                            <Typography 
+                                                variant='h6'
+                                                fontSize={{ xs: '0.8rem', sm: '1.2rem'}}
+                                            >
+                                                {item.time.slice(16,25)}
+                                            </Typography>
+                                            <Typography 
+                                                variant='h6'
+                                                fontSize={{ xs: '0.8rem', sm: '1.2rem'}}
+                                            >
+                                                <span>Total Amount: </span> 
+                                                {item.totalAmount}
+                                            </Typography>
+                                        </Box>
+                                    </Box>
+                                )
+                            })}
+                        </DialogContent>
+                        <DialogActions>
+                            <Button onClick={() => {
+                                setOpen(false);
+                            }} color="primary" autoFocus>
+                                Close
+                            </Button>
+                        </DialogActions>
+                    </Dialog>
                 </Box>
             </Drawer>
             <div className={classes['main-image']}>
